5139cceff7bd27fadf191a9958ea815f82792dfe
Oliver <oliverfruergaard@gmail.com>
2025-03-23 15:11:55 +0100

    update cors origin

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    origin: "https://news-planner.vercel.app/",
+    origin: "http://news-planner-qrvc85ihn-ofgaards-projects.vercel.app/",
   })
 );
 
